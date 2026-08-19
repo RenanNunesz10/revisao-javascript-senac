7165a3b9259bb173abcb086589d634d3e05c2999
Renan Nunes <142848799+RenanNunesz10@users.noreply.github.com>
2026-08-18 21:08:28 -0300

    feat(ex8.js): resolução do exercício 8

--- 08-catalogo-produtos-arrays-objetos/script.js
+++ 08-catalogo-produtos-arrays-objetos/script.js
@@ -12,9 +12,72 @@
 // categoria
 // emEstoque
 
+const produtos = [
+  {
+    id: 1,
+    nome: "Notebook",
+    preco: 3500.00,
+    categoria: "Eletrônicos",
+    emEstoque: true
+  },
+  {
+    id: 2,
+    nome: "Cadeira Gamer",
+    preco: 850.50,
+    categoria: "Móveis",
+    emEstoque: false
+  },
+  {
+    id: 3,
+    nome: "Teclado Mecânico",
+    preco: 250.00,
+    categoria: "Eletrônicos",
+    emEstoque: true
+  },
+  {
+    id: 4,
+    nome: "Garrafa Térmica",
+    preco: 89.90,
+    categoria: "Acessórios",
+    emEstoque: true
+  },
+  {
+    id: 5,
+    nome: "Mousepad Gigante",
+    preco: 45.00,
+    categoria: "Acessórios",
+    emEstoque: false
+  }
+];
+
 // TODO:
 // Percorra o array com for, for...of ou forEach().
+
+//in - acessa os índices do array
+//for (prod in produtos){
+//  console.log(prod)
+//}
+
+
+//in - acessa os valores do array
+//for (prod of produtos){
+//  console.log(prod)
+//}
+
+//for each - para cada elemento
+//function showProducts(prod){
+//  console.log(prod)
+//}
+
+//produtos.forEach(showProducts);
+
 
 // TODO:
 // Para cada item, mostre no console uma frase formatada
 // contendo as informações do produto.
+
+function produtosFormatados(prod){
+  console.log(`Nome: ${prod.nome} \n Preço: R$ ${prod.preco} \n Categoria: ${prod.categoria} \n Em Estoque? ${prod.emEstoque ? "Sim" : "Não"}`)
+}
+
+produtos.forEach(produtosFormatados);
